28baa57f86a0486e6d24b365c581a657cea05865
amulya prabhat <amupraba@amulyas-MacBook-Pro.local>
2024-10-22 22:54:19 +0530

    Added test cases for frames,windows usecase

--- src/main/resources/CustomClass.java
+++ src/main/resources/CustomClass.java
@@ -2,16 +2,10 @@ package main.resources;
 
 import in.precisiontestautomation.drivers.DriverManager;
 import in.precisiontestautomation.utils.WebKeyInitializers;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.awt.*;
-import java.awt.event.KeyEvent;
-import java.time.Duration;
 
 /**
  * @author PTA-dev
@@ -33,5 +27,20 @@ public class CustomClass {
 
     public static void main(String[] args) {
         System.out.println("gmail_sanity".contains("gmail"));
+    }
+
+    public void printFrameText(){
+        System.out.println(WebKeyInitializers.getGlobalVariables().get().get("frameOneText"));
+    }
+    public void printMainFrameText(){
+        System.out.println(WebKeyInitializers.getGlobalVariables().get().get("mainFrameText"));
+    }
+
+    public void printNewTabText(){
+        System.out.println(WebKeyInitializers.getGlobalVariables().get().get("newWindowText"));
+    }
+
+    public void printMainTabText(){
+        System.out.println(WebKeyInitializers.getGlobalVariables().get().get("mainWindowText"));
     }
 }
